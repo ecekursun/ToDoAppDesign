55c9ecfb4b41a1197ce635f2815dd8771d6f3265
Ece Kurşun <67229702+ecekursun@users.noreply.github.com>
2025-04-20 18:32:10 +0300

    Add files via upload

---
+++ TodoAppDesign/TodoAppDesign/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TodoAppDesign
+//
+//  Created by Ece Kurşun on 20.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var yapilacakTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoAppDesign/TodoAppDesign/YapilacakHucreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  YapilacakHucreTableViewCell.swift
+//  TodoAppDesign
+//
+//  Created by Ece Kurşun on 20.04.2025.
+//
+
+import UIKit
+
+class YapilacakHucreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
